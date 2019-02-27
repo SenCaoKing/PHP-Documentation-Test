698056b44fab8f4d1b23ee0ef7464dacc2f3beb1
SenCaoKing <1134349927@qq.com>
2019-02-27 21:19:19 +0800

    生成器 - 生成器语法(斐波那契数列)

--- generators/generator_syntax.php
+++ generators/generator_syntax.php
@@ -230,3 +230,33 @@ echo $gen->getReturn(); // 10
 /**
  * Output: 1 2 3 4 5 6 7 8 9 10
  */
+
+echo "<br/>****************************************<br/>";
+
+/**
+ * 斐波那契数列
+ */
+function getFibonacci()
+{
+    $i = 0;
+    $k = 1; // first fibonacci value
+    yield $k;
+    while (true)
+    {
+        $k = $i + $k;
+        $i = $k - $i;
+        yield $k;
+    }
+}
+
+$y = 0;
+
+foreach(getFibonacci() as $fibonacci)
+{
+    echo $fibonacci . "\n";
+    $y++;
+    if($y > 30)
+    {
+        break; // infinite loop prevent
+    }
+}
